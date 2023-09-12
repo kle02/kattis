7859d887bbea8140445fa5ea4e9abcc24cb5d5ec
kle02 <144286840+kle02@users.noreply.github.com>
2023-09-12 05:12:48 -0400

    Measure Unnar’s level of uncertainty
    Unnar is sure of many things in his life. It is in fact so rare that Unnar is uncertain, that it is important to measure his level of uncertainty when he is. He says ‘u’ when he is uncertain. Unnar’s level of uncertainty is measured by the duration of his ‘u’. For example, if Unnar says ‘uuuuu’, then his level of uncertainty is 5.
    
    https://open.kattis.com/problems/ovissa

---
+++ Unnar.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Unnar {
+	public static void main(String[] args) {
+		
+		Scanner keyboard = new Scanner(System.in);
+	
+		String uncertainty = keyboard.nextLine();
+		
+		System.out.println(uncertainty.length());
+	}
+}
